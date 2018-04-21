7f15d5b622b3151094419a6ed83b20639707f32d
Andrey Yaromenok <ayaromenok@gmail.com>
2018-04-21 17:05:06 +0200

    #8 Svg export tr translation fix

--- librecad/src/ui/forms/qg_dlgoptionsmakercam.cpp
+++ librecad/src/ui/forms/qg_dlgoptionsmakercam.cpp
@@ -34,7 +34,7 @@ QG_DlgOptionsMakerCam::QG_DlgOptionsMakerCam(QWidget* parent, bool modal, Qt::Wi
     this->gbEllipses->setToolTip(tr("MakerCAM as of March 2015 cannot display ellipses and ellipse arcs correctly, when they are created using the <ellipse> tag  with a rotation in the <transform> attribute or as <path> using elliptic arc segments."));
     this->gbImages->setToolTip(tr("Exported images can be useful in SVG editors (Inkscape, etc), but avoided in some CAM's."));
     this->gbDashLines->setToolTip(tr("Many CAM's(MakerCAM, EleskCAM, LaserWeb) ignore dashed/doted line style, which can be useful in lasercut of plywood or for papercraft. "));
-    this->dSpinBoxDefaultElementWidth->setToolTip("tr(Default width of elements can affect some CAM's/SVG Editors, but ignored by other)");
+    this->dSpinBoxDefaultElementWidth->setToolTip(tr("Default width of elements can affect some CAM's/SVG Editors, but ignored by other"));
     this->dSpinBoxDashLinePatternLength->setToolTip(tr("Length of line pattern related to zoom, so default step value required for baking"));
 
     loadSettings();
